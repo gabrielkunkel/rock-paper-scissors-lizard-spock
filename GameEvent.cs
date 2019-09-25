da20b0541c72ed7356ab14d04c16ac4922f3c381
Gabriel Kunkel <henrysaltandson@gmail.com>
2019-09-25 11:29:38 -0500

    initial commit

---
+++ GameEvent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+    class GameEvent
+    {
+        string eventText;
+
+        public GameEvent(string eventText)
+        {
+            this.eventText = eventText;
+        }
+
+        // RunEvent
+
+        // Prompt
+
+        // GetResponse
+
+        public void Print()
+        {
+            Console.WriteLine(eventText);
+        }
+
+        public void PrintEach(string text)
+        {
+            Console.Write(text + " ");
+        }
+    }
+}
